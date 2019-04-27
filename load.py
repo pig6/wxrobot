117f605297a4c6258790977e8a4e0bb4a5075cf1
pig <pig2@qq.com>
2019-04-27 15:50:45 +0800

    fix bug：管理员名称空报错

--- load.py
+++ load.py
@@ -5,14 +5,17 @@ import config
 
 def load_config_to_bot(bot):
     """加载配置项"""
+    has_master = False
     bot_status = '机器人登录成功！！！'
     # 定义远程管理主人 (用于远程管理)
-    master = search_friend(bot, config.bot_master_name)
-    if master:
-        bot.master = master
-        bot_status += '\n机器人管理员成功设置为：「{0}」，这里查看管理员命令手册->' \
-                      'https://github.com/pig6/wxrobot\n\n'.format(config.bot_master_name)
-    else:
+    if config.bot_master_name:
+        master = search_friend(bot, config.bot_master_name)
+        if master:
+            has_master = True
+            bot.master = master
+            bot_status += '\n机器人管理员成功设置为：「{0}」，这里查看管理员命令手册->' \
+                          'https://github.com/pig6/wxrobot\n\n'.format(config.bot_master_name)
+    if not has_master:
         bot.master = bot.file_helper
         bot_status += '\n机器人管理员未成功设置，信息将发送至文件助手，不能使用远程命令控制机器人！\n\n'
     # 设置开关
